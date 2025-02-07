9b93f9d132877f540ef95432146776ba0febc183
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2025-02-07 14:47:41 +0530

    Build Error 2.0

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.externals.push({
-      'node:crypto': 'commonjs crypto',
-    });
-    return config;
+   webpack: (config) => {
+     config.externals.push({
+       'node:crypto': 'crypto',
+     });
+     return config;
   },
 };
 
